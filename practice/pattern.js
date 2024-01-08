1a58a15b21bb8cbe34f1f1419249ae1932641411
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-01-08 15:02:39 +0000

    pattern

---
+++ practice/pattern.js
@@ -0,0 +1,126 @@
+/*
+for(i=1 ; i<=5 ; i++){
+    for(j=1;j<=i;j++){
+        // console.log("*");
+        process.stdout.write("*")
+    }
+    process.stdout.write("\n")
+}
+*/
+/*
+for(i=1 ; i<5 ; i++){
+    for(j=5;j>=i;j--){
+        // console.log("*");
+        process.stdout.write("*")
+    }
+    process.stdout.write("\n")
+}
+
+// 2nd method
+for(i=1 ; i<=5 ; i++){
+    for(j=i;j<=5;j++){
+        // console.log("*");
+        process.stdout.write("*")
+    }
+    process.stdout.write("\n")
+}
+*/
+
+/*
+//using function
+let p = (n)=>{
+    for(i=1;i<n;i++){
+        for(j=1;j<=i;j++){
+            process.stdout.write("*")
+        }
+        process.stdout.write("\n")
+    }
+}
+p(5);
+*/
+
+
+/*
+for(i=1;i<=5;i++){
+    // for(j=5;j>=i;i--)
+    for(j=i;j<=5;j++){
+        process.stdout.write("  ")  //two space
+    }
+    for(k=1;k<=i;k++){
+        process.stdout.write("* ")
+    }
+    process.stdout.write("\n")
+}
+*/
+
+
+/*
+for(i=1;i<=5;i++){
+    // for(j=5;j>=i;i--)
+    for(j=1;j<=i;j++){
+        process.stdout.write("  ")  //two space
+    }
+    for(k=i;k<=5;k++){
+        process.stdout.write("* ")
+    }
+    process.stdout.write("\n")
+}
+*/
+
+/*
+for(i=1;i<=5;i++){
+    for(j=i;j<=5;j++){
+        process.stdout.write("  ")
+    }
+    for(k=1;k<i;k++){
+        process.stdout.write("* ")
+    }
+    for(l=1;l<=i;l++){
+            process.stdout.write("* ")
+    }
+        process.stdout.write("\n")
+}
+
+*/
+
+/*
+for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        process.stdout.write("  ")
+    }
+    for(k=i;k<5;k++){
+        process.stdout.write("* ")
+    }
+    for(l=i;l<=5;l++){
+            process.stdout.write("* ")
+    }
+        process.stdout.write("\n")
+}
+
+*/
+
+for(i=1;i<5;i++){
+    for(j=i;j<=5;j++){
+        process.stdout.write("  ")
+    }
+    for(k=1;k<i;k++){
+        process.stdout.write("* ")
+    }
+    for(l=1;l<=i;l++){
+            process.stdout.write("* ")
+    }
+        process.stdout.write("\n")
+}
+for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        process.stdout.write("  ")
+    }
+    for(k=i;k<5;k++){
+        process.stdout.write("* ")
+    }
+    for(l=i;l<=5;l++){
+            process.stdout.write("* ")
+    }
+        process.stdout.write("\n")
+}
+
