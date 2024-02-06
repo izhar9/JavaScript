b667bc3d2162210f48ad9ed28043394c870a1625
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-02-06 05:34:06 +0000

    static

---
+++ 09_classes_and_oop/staticprop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){  // kahi baar hum chahte hai ki hum kisi method ka access har us object ko n de jo is class se instanciate(class se bana hai) huaa hai. static keyword us method ya properties ko access hone se rokta hai.
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
