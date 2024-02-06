e2bb91d1a92785b1b3873dd7d592b2aa900e1ea3
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-02-06 05:10:52 +0000

    call

--- 09_classes_and_oop/call.js
+++ 09_classes_and_oop/call.js
@@ -11,5 +11,5 @@ function createUser(username, email, password){
     this.password = password
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
-console.log(chai);
+const izhar = new createUser("izhar", "izhar@fb.com", "123")
+console.log(izhar);
