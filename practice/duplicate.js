e5fc798e8d6ccd81d54f6e417972d0f6320dde80
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-03-28 15:55:39 +0530

    Update duplicate.js

--- practice/duplicate.js
+++ practice/duplicate.js
@@ -1,8 +1,16 @@
-// How To find duplicate elements in array
+// find duplicate elements in array
 
 let array = [2,3,4,5,3,1,2,4,3,5]
 
 let duplicatearray = array.filter((val,index,arr) =>{
+
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+
+// The filter() method does not execute the function for empty elements.
+
+// The filter() method does not change the original array.
+
+    
     return arr.indexOf(val) !== index
 })
 // console.log(duplicatearray);
